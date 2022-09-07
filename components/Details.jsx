5ae8805869ce5d5c5e9f7276c05fb9bd770707bd
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-09-07 12:53:26 -0700

    finished application

--- components/Details.jsx
+++ components/Details.jsx
@@ -1,12 +1,102 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { ScrollView, Text, View, StyleSheet } from "react-native";
 
-const Details = () => {
+const Details = ({ route }) => {
+  const { recipe } = route.params;
+
   return (
-    <Text>
-      Hello from details
-    </Text>
-  )
-}
+    <ScrollView>
+      <View style={styles.details}>
 
-export default Details
+      <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Recipe name:
+          </Text>
+          <Text style={styles.ingredients}>
+          {`${recipe.label}`}
+          </Text>
+        </View>
+        
+        <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Ingredients:
+          </Text>
+          <Text style={styles.ingredients}>
+            {`${recipe.ingredients.map((item) => item["food"])}`}
+          </Text>
+        </View>
+
+        <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Calories: 
+          </Text>
+          <Text style={styles.ingredients}>
+          {Math.round(`${recipe.calories}`)}
+          </Text>
+        </View>
+
+        <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Meal Type:
+          </Text>
+          <Text style={styles.ingredients}>
+          {`${recipe.mealType}`}
+          </Text>
+          
+        </View>
+
+        <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Description:
+          </Text>
+          <Text style={styles.ingredients}>
+          {`${recipe.ingredientLines}`}
+          </Text>
+        </View>
+
+        <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Diet Label:
+          </Text>
+          <Text style={styles.ingredients}>
+          {`${recipe.dietLabels}`}
+          </Text>
+        </View>
+
+        <View style={styles.item}>
+          <Text style={{fontSize: 22, color: "#FF5A5F", fontWeight: "800"}}>
+            Cuisine Type:
+          </Text>
+          <Text style={styles.ingredients}>
+          {`${recipe.cuisineType}`}
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  details: {
+    marginBottom: 30,
+    padding: 5
+  },
+  ingredients: {
+    fontSize: 20,
+    color: "#FF5A5F"
+  },
+  item: {
+    shadowColor: "#FF5A5F",
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 20,
+    backgroundColor: "white",
+    margin: 10,
+    padding: 10,
+    flexDirection: "column"
+  }
+});
+
+export default Details;
--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,7 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Text, Keyboard, StyleSheet, View, TextInput, TouchableOpacity, SafeAreaView, ActivityIndicator, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  Keyboard,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+  Image,
+} from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   const [recipes, setRecipes] = useState();
   const [searchQuery, setSearchQuery] = useState(" ");
   const [numberOfRecipes, setNumberOfRecipes] = useState("1");
@@ -25,60 +36,83 @@ const HomeScreen = () => {
   useEffect(() => {
     setLoading(true);
     apiCall();
-  },[]);
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 23, fontWeight: "800", width: "90%", color: "#FF5A5F"}}>
-        What Recipe would you like to search?
+      <Text
+        style={{
+          fontSize: 23,
+          fontWeight: "800",
+          width: "90%",
+          color: "#FF5A5F",
+          marginBottom: 10
+        }}
+      >
+        Search a recipe!
       </Text>
-      <View style={{display: 'flex', flexDirection: "row",}}>
-        <TextInput placeholder='search a recipe...'
-        style={styles.inputField}
-        onChangeText={text => setSearchQuery(text)}
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        <TextInput
+          placeholder="search a recipe..."
+          style={styles.inputField}
+          onChangeText={(text) => setSearchQuery(text)}
         />
 
         <TextInput
-        onChangeText={text => setNumberOfRecipes(text)}
-        style={[styles.inputField, {width: "20%", fontSize: 18, marginLeft: 15, color: "white", fontWeight: "bold"}]}
-        value={numberOfRecipes}
-        keyboardType="number-pad"
+          onChangeText={(text) => setNumberOfRecipes(text)}
+          style={[
+            styles.inputField,
+            {
+              width: "20%",
+              fontSize: 18,
+              marginLeft: 15,
+              color: "white",
+              fontWeight: "bold",
+            },
+          ]}
+          value={numberOfRecipes}
+          keyboardType="number-pad"
         />
       </View>
-      <TouchableOpacity style={styles.button}
-      onPress={apiCall}
-      title="submit">
-        <Text style={styles.buttonText}>
-          Search
-        </Text>
+      <TouchableOpacity style={styles.button} onPress={apiCall} title="submit">
+        <Text style={styles.buttonText}>Search</Text>
       </TouchableOpacity>
-      <SafeAreaView style={{flex: 1}}>
-        {loading? <ActivityIndicator size="large" color="black" /> :
-       <FlatList
-        style={styles.recipes}
-        data={recipes}
-        renderItem={({ item }) => (
-          <View style={styles.recipes}>
-            <Image style={styles.img}
-            source={{uri: `${item.recipe.image}`}}
+      <SafeAreaView style={{ flex: 1 }}>
+        {loading ? (
+          <ActivityIndicator size="large" color="black" />
+        ) : (
+          <FlatList
+            style={styles.recipes}
+            data={recipes}
+            renderItem={({ item }) => (
+              <View style={styles.recipes}>
+                <Image
+                  style={styles.img}
+                  source={{ uri: `${item.recipe.image}` }}
+                />
+                <View style={{ padding: 20, flexDirection: "row" }}>
+                  <Text style={styles.label}>{item.recipe.label}</Text>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Details", { recipe: item.recipe });
+                    }}
+                  >
+                    <Text
+                      style={{ marginLeft: 50, fontSize: 20, color: "#FF5A5F" }}
+                    >
+                      Details
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
           />
-          <View style={{padding: 20, flexDirection: "row"}}>
-            <Text style={styles.label}>
-              {item.recipe.label}
-            </Text>
-            <TouchableOpacity onPress={() => {}}>
-              <Text style={{marginLeft: 50, fontSize: 20, color: "#FF5A5F"}}>
-                Details
-              </Text>
-            </TouchableOpacity>
-          </View>
-          </View>
         )}
-      keyExtractor={(item, index) => index.toString()} />}
       </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -86,7 +120,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
   inputField: {
     height: "120%",
@@ -95,10 +129,10 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginTop: 10,
     paddingLeft: 15,
-    color: "white"
+    color: "white",
   },
   buttons: {
-    flexDirection: "row"
+    flexDirection: "row",
   },
   button: {
     backgroundColor: "#FF5A5F",
@@ -108,35 +142,35 @@ const styles = StyleSheet.create({
     height: 35,
     borderRadius: 15,
     justifyContent: "center",
-    marginTop: 25
+    marginTop: 25,
   },
   buttonText: {
     color: "white",
     fontSize: 20,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   img: {
     width: "100%",
     height: 200,
-    borderRadius: 20
+    borderRadius: 20,
   },
   label: {
     fontSize: 15,
     width: "60%",
     color: "#FF5A5F",
-    fontWeight: "700"
+    fontWeight: "700",
   },
   recipes: {
     shadowColor: "#FF5A5F",
     shadowOpacity: 0.5,
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 20,
     backgroundColor: "white",
     margin: 10,
-    marginBottom: 40
-  }
-})
+    marginBottom: 40,
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
